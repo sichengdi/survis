0b5fa7b472680162eda6990c77f5544e98c0c8c7
sichengdi <dissok@qq.com>
2025-05-15 19:20:58 +0100

    Update tag_categories.js

--- data/tag_categories.js
+++ data/tag_categories.js
@@ -1,7 +1,38 @@
 define({
-	tagCategories: {
-		"type": {
-			"description": "type of the paper"
-		}
-	}
+  tagCategories: [
+    {
+      name: "Type",
+      prefix: "type:",
+      tags: [
+        "type:tool",
+        "type:survey",
+        "type:taxonomy",
+        "type:study"
+      ]
+    },
+    {
+      name: "Application",
+      prefix: "application:",
+      tags: [
+        "application:bibliometric",
+        "application:trend-analysis",
+        "application:evaluation",
+        "application:decision-making",
+        "application:task-analysis"
+      ]
+    },
+    {
+      name: "Topic",
+      prefix: "topic:",
+      tags: [
+        "topic:network",
+        "topic:citation",
+        "topic:text-visualization",
+        "topic:visual-literacy",
+        "topic:survey-analysis",
+        "topic:resources",
+        "topic:book-review"
+      ]
+    }
+  ]
 });
